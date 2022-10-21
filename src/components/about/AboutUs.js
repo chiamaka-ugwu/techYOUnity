c2a3181740d3c2781924054d33305de966c0f385
Chiamaka Ugwu <mannehills@gmail.com>
2022-10-21 20:40:03 +0100

    Update

--- src/components/about/AboutUs.js
+++ src/components/about/AboutUs.js
@@ -34,24 +34,26 @@ const AboutUs = () => {
       </div>
 
       <div className="about-us">
-        <h3>About Us</h3>
-        <h2>
-          Our Differences <br />{" "}
-          <span className="excel">makes us stronger</span>
-        </h2>
-        <p>
-          The Notion of Tech being the easy way out has put many people in
-          situations beyond their control as the gap between starting out in
-          tech and becoming successful in Tech is very wide. In between this gap
-          comes a lot of confusion, sacrifices, misconceptions, obstacles,
-          principles to be learned, fears, perseverance, setbacks, unlearning,
-          learning and relearning, etc. <br /> <br />
-          But don't panic, techYOUnity has been designed to bridge the gap. We
-          are set up to be a pillar that you can lean on as you embark on the
-          journey to be an EXCELLENT TECHIE. Join our vehicle, we would drive
-          you through the potholes and roadblocks to take you to the land of
-          EXCELLENT TECHIES.
-        </p>
+        <div className="about-content">
+          <h3>About Us</h3>
+          <h2>
+            Our Differences <br className="br" />
+            <span className="excel">makes us stronger</span>
+          </h2>
+          <p>
+            The Notion of Tech being the easy way out has put many people in
+            situations beyond their control as the gap between starting out in
+            tech and becoming successful in Tech is very wide. In between this
+            gap comes a lot of confusion, sacrifices, misconceptions, obstacles,
+            principles to be learned, fears, perseverance, setbacks, unlearning,
+            learning and relearning, etc. <br /> <br />
+            But don't panic, techYOUnity has been designed to bridge the gap. We
+            are set up to be a pillar that you can lean on as you embark on the
+            journey to be an EXCELLENT TECHIE. Join our vehicle, we would drive
+            you through the potholes and roadblocks to take you to the land of
+            EXCELLENT TECHIES.
+          </p>
+        </div>
       </div>
       <div className="img-cont">
         <img src={friends} alt="friends" />
